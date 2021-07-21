2d0228c3c85785b8a4eb6f25e84d6c0d450aa91b
lucioerlan <luciodevinfo@gmail.com>
2021-07-21 17:55:29 -0300

    :sparkles: feat adding logic to get repos

--- src/modules/get-org/get-org-models.js
+++ src/modules/get-org/get-org-models.js
@@ -1,8 +1,50 @@
-const getOrg = async () => {
+const api = require('../../config/api');
+const { logger } = require('../../middlewares');
 
-  return 'hello world';
+/**
+ *
+ * @function getOrg
+ * @returns {Promise<Object>}
+ */
+
+const getOrg = async () => {
+  try {
+    const {
+      data
+    } = await api.get('takenet/repos?type=owner&page=1&per_page=100');
+
+    const reponse = data.map((repo) => {
+      return {
+        name: repo.name,
+        description: repo.description,
+        language: repo.language,
+        created_at: repo.created_at,
+      };
+    });
+
+    return validateObject(reponse);
+  } catch (err) {
+    logger.error('request in error!');
+  }
 };
 
+/**
+ *
+ * @param {Object} reponse
+ */
+
+const validateObject = async (reponse) => {
+  try {
+    return reponse.slice(0, 5).sort(({created_at, language }) => {
+      if (language === 'C#')
+      return new Date(created_at) - new Date(created_at);
+    });
+  } catch (err) {
+    logger.error('error in validating!');
+  }
+};
+
+
 module.exports = {
-  getOrg,
+  getOrg
 };
