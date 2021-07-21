2978c5ca5aae9057385f8f1c68c368031205292c
lucioerlan <luciodevinfo@gmail.com>
2021-07-21 17:54:48 -0300

    :sparkles: feat adding caching

--- src/modules/get-org/get-org-routes.js
+++ src/modules/get-org/get-org-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const OrgController = require('./get-org-controller');
+const { cachingMiddleware } = require('../../middlewares');
 
 const orgController = new OrgController();
 
-router.get('/get-repos-take', orgController.show);
+router.get('/get-repos-take', cachingMiddleware.route(), orgController.show);
 
 module.exports = router;
