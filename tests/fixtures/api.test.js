b585034388322a8c93d797781a3915e0b54c5d46
lucioerlan <luciodevinfo@gmail.com>
2021-07-21 12:16:12 -0300

    :heavy_check_mark: test logger request

--- tests/fixtures/api.test.js
+++ tests/fixtures/api.test.js
@@ -1,12 +1,13 @@
 const api = require('../../src/config/api');
+const { logger } = require('../../src/middlewares');
 
 const getOrg = async () => {
   return api
     .get('takenet/repos?type=owner&page=1&per_page=1')
     .then(res => res)
-    .catch(error => console.log(error));
+    .catch(error => logger.error(error));
 };
 
 module.exports = {
-  getOrg
+  getOrg,
 };
