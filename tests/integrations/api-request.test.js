5bddf6fb6e0f30cd5a65d6bdc339583fe2f92fff
lucioerlan <luciodevinfo@gmail.com>
2021-07-22 04:21:24 -0300

    :hammer: refactor catch many request

--- tests/integrations/api-request.test.js
+++ tests/integrations/api-request.test.js
@@ -18,42 +18,41 @@ describe('Get ORG TakeNet', () => {
   });
 
   it('should return the correct data', () => {
-    getOrg().then(({ data, status }) => {
-      try {
+    getOrg()
+      .then(({ data, status }) => {
         expect(status).to.equal(200);
         expect(data).to.have.all.keys('name', 'url', 'id', 'logo');
-      } catch (err) {
+      })
+      .catch((err) => {
         expect(err.status).to.equal(403);
         expect(err.response.data.message).to.equal(
           'Rate limit exceeded. Try again in a few minutes.',
         );
-      }
-    });
+      });
   });
 
   it('should return an array of 1 object', () => {
-    return getOrg().then(({ data, status }) => {
-      try {
+    return getOrg()
+      .then(({ data, status }) => {
         expect(data).to.be.an('array');
         expect(status).to.equal(200);
-        expect(data).to.have.lengthOf(1);
         expect(data[0].owner.id).to.be.a('number');
         expect(data[0].owner.login).to.be.a('string');
         expect(data[0].owner.avatar_url).to.be.a('string');
         expect(data[0].owner.html_url).to.be.a('string');
         expect(data[0].owner.type).to.be.a('string');
-      } catch (err) {
+      })
+      .catch((err) => {
         expect(err.status).to.equal(403);
         expect(err.response.data.message).to.equal(
           'Rate limit exceeded. Try again in a few minutes.',
         );
-      }
-    });
+      });
   });
 
   it('should must return information about the company to takeNet', () => {
-    return getOrg().then(({ data, status }) => {
-      try {
+    return getOrg()
+      .then(({ data, status }) => {
         // expect an object back
         expect(typeof data).to.equal('object');
         expect(status).to.equal(200);
@@ -64,12 +63,12 @@ describe('Get ORG TakeNet', () => {
         );
         expect(data[0].owner.html_url).to.equal('https://github.com/takenet');
         expect(data[0].owner.type).to.equal('Organization');
-      } catch (err) {
+      })
+      .catch((err) => {
         expect(err.status).to.equal(403);
         expect(err.response.data.message).to.equal(
           'Rate limit exceeded. Try again in a few minutes.',
         );
-      }
-    });
+      });
   });
 });
