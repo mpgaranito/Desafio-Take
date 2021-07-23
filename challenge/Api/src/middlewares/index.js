9d82cd21758e07462fd9d68a7aff2470e82842c8
lucioerlan <luciodevinfo@gmail.com>
2021-07-22 22:24:57 -0300

    :sparkles: feat call middlewares

--- challenge/Api/src/middlewares/index.js
+++ challenge/Api/src/middlewares/index.js
@@ -1,9 +1,11 @@
 const { logger } = require('./logger');
 const cachingMiddleware  = require('./caching');
+const { securityMiddleware } = require('./security');
 const { responseMiddleware } = require('./response');
 
 module.exports = {
   logger,
   cachingMiddleware,
+  securityMiddleware,
   responseMiddleware
 };
